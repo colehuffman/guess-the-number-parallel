10f4bce8660d8c5a6e6b36c33c88b6f0014355be
colehuffman <79929352+colehuffman@users.noreply.github.com>
2021-03-27 11:07:19 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,57 @@
-// when A is pressed the guess variable increases by 1
+// when A is pressed the number variable increases by 1
 input.onButtonPressed(Button.A, function () {
-    guess += 1
-    basic.showString("" + (guess))
+    // increases the number variable by 1
+    number += 1
+    // displays number
+    basic.showString("" + (number))
 })
 input.onButtonPressed(Button.AB, function () {
     // if the guessed number is equal to the secret number a check is displayed
-    // if the guessed number is below the secret number the word "add' is displayed
-    // if the guessed number is above the secret number the word "subtract' is displayed
-    if (guess == secretnumber) {
+    // if the guessed number is less than the secret number the word "add' is displayed
+    // if the guessed number is greater than the secret number the word "subtract' is displayed
+    if (number == secretnumber) {
+        // displays a check
         basic.showIcon(IconNames.Yes)
+        // pauses for 1 second so the viewer can see the check
         basic.pause(1000)
         // resets the entire program when the secret number is guessed
         control.reset()
-    } else if (guess < secretnumber) {
+    } else if (number < secretnumber) {
+        // displays "add"
         basic.showString("add")
-    } else if (guess > secretnumber) {
+    } else if (number > secretnumber) {
+        // displays "subtract"
         basic.showString("subtract")
     }
+    // increases guesses count by 1
+    guesses += 1
+    // if the wrong number is guessed 3 times the system tells you what the correct number was and then resets
+    if (guesses == 3) {
+        basic.showString("Good Try! the number was")
+        // displays the secretnumber variable
+        basic.showString("" + (secretnumber))
+        // resets
+        control.reset()
+    }
 })
-// when B is pressed the guess variable decreases by 1
+// when B is pressed the number variable decreases by 1
 input.onButtonPressed(Button.B, function () {
-    guess += -1
-    basic.showString("" + (guess))
+    // decreases the number variable by 1
+    number += -1
+    // sets the number to its absolute value. this stops the number from becoming negative
+    number = Math.abs(number)
+    // displays number
+    basic.showString("" + (number))
 })
-let guess = 0
+// tells the user how many guesses are left until the system resets
+input.onGesture(Gesture.Shake, function () {
+    // calculates the guesses left before reset
+    basic.showString("" + (3 - guesses))
+    // displays "Guesses left" so that the user knows what the number meant
+    basic.showString("Guesses left")
+})
+let guesses = 0
+let number = 0
 let secretnumber = 0
-// sets the secret number to pick a random number between 0 and 25
-secretnumber = randint(0, 25)
-// stops guessed number from passing 0 or 25
-guess = Math.constrain(guess, 0, 25)
+// on start the secretnumber variable is set to a random number from 0 to 25
+secretnumber = randint(0, 20)
